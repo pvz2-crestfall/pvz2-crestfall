a235298d228780bf4eb72d277a803d40ac737388
ZackGabri <44709674+ZackGabri@users.noreply.github.com>
2026-04-01 16:59:10 +0200

    Prospector zombie hp 190 -> 270

---
+++ scripts/filter_quests.js
@@ -0,0 +1,29 @@
+import { readFileSync, writeFileSync } from 'fs';
+
+const quests = JSON.parse(readFileSync('QUESTS.json'));
+
+console.log(quests.objects[0].objdata.Quests.length);
+
+const categories = new Set();
+const themes = new Set();
+quests.objects[0].objdata.Quests = quests.objects[0].objdata.Quests.filter((quest) => {
+    quest = Object.values(quest)[0];
+
+    const filteredCategories = ['Unused', 'DailyPinataHunt', 'PremiumPlant'];
+    const filteredThemes = ['Rift', 'Arena', 'Ads', 'LevelUp', 'PremiumSeeds', 'PremiumPlant', 'LevelOfTheDay'];
+
+    categories.add(quest.QuestCategory);
+    themes.add(quest.OverrideQuestTheme);
+
+    if (filteredCategories.includes(quest.QuestCategory)) return false;
+    if (filteredThemes.includes(quest.OverrideQuestTheme)) return false;
+    if (quest.SKUs != undefined) return false;
+    if (quest.Disabled) return false;
+    if (quest.Slot == 'Unused') return false;
+
+    return true;
+});
+
+console.log(quests.objects[0].objdata.Quests.length);
+console.log(categories, themes);
+writeFileSync('QUESTS.new.json', JSON.stringify(quests, null, 4));
